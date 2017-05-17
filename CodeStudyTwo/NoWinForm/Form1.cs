2e5716dba765fed5360c809c7bd0f40df8e15174
hy <baichuan@hotmail.com>
2017-05-17 15:53:24 +0800

    hy <baichuan@hotmail.com>

---
+++ CodeStudyTwo/NoWinForm/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NoWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //MessageBox.Show("this");
+            //if (MessageBox.Show("this") == DialogResult.OK)
+            //{
+            //    Application.Exit();
+            //}
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            Timer t = new Timer();
+            t.Tick += t_Tick;
+            t.Interval = 10000;
+            t.Start();
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.Hide();
+        }
+    }
+}
---
+++ CodeStudyTwo/ReportTest/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportTest
+{
+    public class Book
+    {
+        public String Name { get; set; }
+        public String Author { get; set; }
+        public String ISBN { get; set; }
+        public String Price { get; set; }
+    }
+}
